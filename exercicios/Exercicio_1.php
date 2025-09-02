de88ec45d5da3dd945dcf3c57673dd66c4683e76
Isac Gouveia <112826087+isacgouveia@users.noreply.github.com>
2025-09-02 18:47:02 -0300

    Update and rename Funcoes&Filtros.php to Exercicio_1.php

--- exercicios/Exercicio_1.php
+++ exercicios/Exercicio_1.php
@@ -3,13 +3,13 @@
         [
             "titulo" => "Meu portfólio",
             "finalizado" => true,
-            "data" => "2025-08-30",
+            "data" => 2024,
             "descricao" => "Meu primeiro portfolio",
         ],
         [
             "titulo" => "Salada de frutas",
             "finalizado" => false,
-            "data" => "2025-08-30",
+            "data" => 2023,
             "descricao" => "Minha salada de frutas",
         ],
     ];
@@ -21,27 +21,13 @@
         return '<span style="red">❌ - Não finalizado</span>';
     }
 
-    /*
-     O nome do parâmetro da função não necessariamente precisa ser o nome da váriavel que está nossa lista de array (projetos)
-
-     $finalizado = null - estamos indicando que a variável $filtrados será sem conteúdo no momento.
-     
-     IS_NULL = indica se o valor é Nulo.
-
-     && (and) - Quando utilizar o and as condições (uma, duas ou mais) precisam ser todas verdadeiras.
-
-     === - Indica que um valor/condição tem que ser EXATAMENTE o outro.
-     */
-    function Filter($p, $finalizado = null){
-        if(is_null($finalizado)){
-            return $p;
-        }
+    function Filter($p, $funcao){
 
         $filtrados = []; // Criamos uma nova variável do tipo array para guardar os projetos.
 
         // Utilizamos o loop para cada um dos projetos.
         foreach ($p as $projeto){
-            if($projeto['finalizado'] === $finalizado){
+            if($funcao($projeto)){
                 // Adicionaremos novos elementos dentro do array o método $var [] = $var
                 $filtrados [] = $projeto; 
             }
@@ -49,6 +35,10 @@
 
         return $filtrados; // Retorna todos os elementos filtrados
     }
+
+    $filtros = filter($projetos, function($projeto){
+        return $projeto['data'] < 2024;
+    });
 ?>
 
 <!DOCTYPE html>
@@ -59,14 +49,11 @@
     <title>Filtros e Funções</title>
 </head>
 <body>
-    <!-- 
-        Precisamos indicar a função de filter dentro do nosso foreach,
-        passando a nossa lista de projetos
 
-        filter($projetos, false) - onde está false indica que o projeto não está finalizado, se caso for true indica que está finalizado.
-    -->
-    <?php foreach(filter($projetos, false) as $projeto):?>
+    <?php foreach($filtros as $projeto):?>
 
+        <div>Titulo: <?= $projeto['titulo'] ?></div>
+        <div>Data: <?= $projeto['data'] ?></div>
         <div>Projeto: <?= Status($projeto) ?></div>
 
         <?php endforeach; ?>
